c9fb302d11b8601ec6dd731e6e4e681d9ceff2cd
Alwin <geraldcheu@gmail.com>
2019-04-05 18:31:15 +0800

    lighting and adjustments

---
+++ Assets/Source/VRPlayerInit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRPlayerInit : MonoBehaviour
+{
+    private Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player").transform;
+        Player.transform.SetParent(GameObject.FindWithTag("Car").transform);
+        Player.transform.localPosition = new Vector3(-0.351f, -0.24f, -0.02f);
+        Player.transform.localEulerAngles = new Vector3(0, -10, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
