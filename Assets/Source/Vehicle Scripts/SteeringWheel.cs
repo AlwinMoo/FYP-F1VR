a0dd17d39ae168ac32fb8a90a8e39b79baf3462d
Alwin <geraldcheu@gmail.com>
2019-04-26 10:32:56 +0800

    steering wheel test update

--- Assets/Source/Vehicle Scripts/SteeringWheel.cs
+++ Assets/Source/Vehicle Scripts/SteeringWheel.cs
@@ -7,9 +7,8 @@ namespace Valve.VR.InteractionSystem
     public class SteeringWheel : MonoBehaviour
     {
         //private Transform Car;
-        private Vector3 offset;
-        
-        private Vector3 defaultWheelAngle;
+        //private Vector3 offset;
+        //private Vector3 defaultWheelAngle;
 
         // Start is called before the first frame update
         void Start()
@@ -24,27 +23,31 @@ namespace Valve.VR.InteractionSystem
             //this.transform.eulerAngles = new Vector3(Car.eulerAngles.x + 15, Car.eulerAngles.y, this.transform.eulerAngles.z);
             //this.transform.position = Car.position;
 
-            if (!CircularDrive.isDrivingActive && !Mathf.Approximately(0.0f, this.transform.rotation.z)) //if none of the hands are holding anything
-            {
-                float deltaAngle = 150 * Time.deltaTime;
-                float wheelAngle = this.transform.rotation.eulerAngles.z;
+            //if (!CircularDrive.isDrivingActive && !Mathf.Approximately(0.0f, this.transform.rotation.z)) //if none of the hands are holding anything
+            //{
+            //    float deltaAngle = 150 * Time.deltaTime;
+            //    float wheelAngle = this.transform.rotation.eulerAngles.z;
 
-                wheelAngle = (wheelAngle > 180) ? wheelAngle - 360 : wheelAngle;
+            //    wheelAngle = (wheelAngle > 180) ? wheelAngle - 360 : wheelAngle;
 
-                if (Mathf.Abs(deltaAngle) > Mathf.Abs(wheelAngle))
-                    wheelAngle = 0f;
-                else if (wheelAngle > 0f)
-                    wheelAngle -= deltaAngle;
-                else
-                    wheelAngle += deltaAngle;
+            //    if (Mathf.Abs(deltaAngle) > Mathf.Abs(wheelAngle))
+            //        wheelAngle = 0f;
+            //    else if (wheelAngle > 0f)
+            //        wheelAngle -= deltaAngle;
+            //    else
+            //        wheelAngle += deltaAngle;
 
-                this.transform.eulerAngles = new Vector3(this.transform.rotation.eulerAngles.x, this.transform.rotation.eulerAngles.y, wheelAngle);
-            }
-        }
+            //    this.transform.eulerAngles = new Vector3(this.transform.rotation.eulerAngles.x, this.transform.rotation.eulerAngles.y, wheelAngle);
+            //}
 
-        public void OnCollisionEnter(Collision collision)
-        {
-            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+            Vector3 tempRotation = this.transform.localEulerAngles;
+
+            tempRotation.z = (tempRotation.z > 180) ? tempRotation.z - 360 : tempRotation.z;
+            tempRotation.z = Mathf.Clamp(tempRotation.z, -160, 160);
+
+            this.transform.localEulerAngles = tempRotation;
+
+            Debug.Log(tempRotation.z);
         }
     }
 }
