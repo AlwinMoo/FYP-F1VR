9dc70e4b92adac29ccb3e5d1d4f0c2f07d63eab6
L-Jun-H <30991824+L-Jun-H@users.noreply.github.com>
2019-05-06 11:31:55 +0800

    Hands Tutorial

--- Assets/Source/MainMenuScripts/TutorialController.cs
+++ Assets/Source/MainMenuScripts/TutorialController.cs
@@ -2,38 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialController : MonoBehaviour
+namespace Valve.VR.InteractionSystem.Sample
 {
-    public GameObject m_goLeftController;
-    public GameObject m_goRightController;
-
-    public GameObject m_goLeftHand;
-    public GameObject m_goRightHand;
-
-    float fTimer =0;
-    public float fTimeLimit = 5;
-
-    // Start is called before the first frame update
-    void Start()
+    public class TutorialController : MonoBehaviour
     {
-    }
+        public GameObject m_goLeftController;
+        public GameObject m_goRightController;
 
-    // Update is called once per frame
-    void Update()
-    {
-            if (fTimer <= fTimeLimit)
-            {
-               m_goLeftHand.SetActive(false);
-               m_goRightHand.SetActive(false);
-            fTimer += Time.deltaTime;
-            }
-            if (fTimer >= fTimeLimit)
-            {
-            m_goLeftHand.SetActive(true);
-            m_goRightHand.SetActive(true);
+        public GameObject m_goLeftHand;
+        public GameObject m_goRightHand;
 
-            m_goLeftController.SetActive(false);
-            m_goRightController.SetActive(false);
+        float fTimer = 0;
+        public float fTimeLimit = 5;
+
+        bool bControlConnected;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            bControlConnected = false;
+        }
+
+        void OnEnable()
+        {
+            Debug.Log("Testing connection for devices");
+            SteamVR_Events.DeviceConnected.Listen(OnDeviceConnected);
+        }
+
+        // A SteamVR device got connected/disconnected
+        private void OnDeviceConnected(int index, bool connected)
+        {
+
+            if (connected)
+            {
+                if (OpenVR.System != null)
+                {
+                    //lets figure what type of device got connected
+                    ETrackedDeviceClass deviceClass = OpenVR.System.GetTrackedDeviceClass((uint)index);
+                    if (deviceClass == ETrackedDeviceClass.Controller)
+                    {
+                        bControlConnected = true;
+                        // Debug.Log("Controller got connected at index:" + index);
+                    }
+                    else if (deviceClass == ETrackedDeviceClass.GenericTracker)
+                    {
+                        // Debug.Log("Tracker got connected at index:" + index);
+                    }
+                }
             }
         }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (bControlConnected == true)
+            {
+                if (fTimer <= fTimeLimit)
+                {
+                    m_goLeftHand.SetActive(false);
+                    m_goRightHand.SetActive(false);
+                    fTimer += Time.deltaTime;
+                }
+                if (fTimer >= fTimeLimit)
+                {
+                    m_goLeftHand.SetActive(true);
+                    m_goRightHand.SetActive(true);
+
+                    m_goLeftController.SetActive(false);
+                    m_goRightController.SetActive(false);
+                }
+            }
+        }
+    }
 }
